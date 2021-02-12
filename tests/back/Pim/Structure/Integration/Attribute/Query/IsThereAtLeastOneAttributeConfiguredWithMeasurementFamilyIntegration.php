2076f570d0647028058b7403d0b2443a6d6aa8ad
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2021-02-12 17:10:40 +0100

    CPM-143: fix tests

--- tests/back/Pim/Structure/Integration/Attribute/Query/IsThereAtLeastOneAttributeConfiguredWithMeasurementFamilyIntegration.php
+++ tests/back/Pim/Structure/Integration/Attribute/Query/IsThereAtLeastOneAttributeConfiguredWithMeasurementFamilyIntegration.php
@@ -53,9 +53,9 @@ VALUES
 SQL;
 
         $insertMetricAttribute = <<<SQL
-INSERT INTO `pim_catalog_attribute` (`id`, `group_id`, `sort_order`, `useable_as_grid_filter`, `max_characters`, `validation_rule`, `validation_regexp`, `wysiwyg_enabled`, `number_min`, `number_max`, `decimals_allowed`, `negative_allowed`, `date_min`, `date_max`, `metric_family`, `default_metric_unit`, `max_file_size`, `allowed_extensions`, `minimumInputLength`, `is_required`, `is_unique`, `is_localizable`, `is_scopable`, `code`, `entity_type`, `attribute_type`, `backend_type`, `properties`, `created`, `updated`)
+INSERT INTO `pim_catalog_attribute` (`id`, `group_id`, `sort_order`, `useable_as_grid_filter`, `max_characters`, `validation_rule`, `validation_regexp`, `wysiwyg_enabled`, `number_min`, `number_max`, `decimals_allowed`, `negative_allowed`, `date_min`, `date_max`, `metric_family`, `default_metric_unit`, `max_file_size`, `allowed_extensions`, `minimumInputLength`, `is_required`, `is_unique`, `is_localizable`, `is_scopable`, `code`, `entity_type`, `attribute_type`, `backend_type`, `properties`, `created`, `updated`, `description`)
 VALUES
-	(7, 3, 1, 1, NULL, NULL, NULL, NULL, NULL, NULL, 1, 0, NULL, NULL, 'Weight', 'KILOGRAM', NULL, '', NULL, 0, 0, 0, 0, :measurementFamily, 'Akeneo\\Pim\\Enrichment\\Component\\Product\\Model\\Product', 'pim_catalog_metric', 'metric', 'a:2:{s:19:\"auto_option_sorting\";N;s:19:\"reference_data_name\";N;}', '2020-03-06 10:41:04', '2020-03-06 10:41:04');
+	(7, 3, 1, 1, NULL, NULL, NULL, NULL, NULL, NULL, 1, 0, NULL, NULL, 'Weight', 'KILOGRAM', NULL, '', NULL, 0, 0, 0, 0, :measurementFamily, 'Akeneo\\Pim\\Enrichment\\Component\\Product\\Model\\Product', 'pim_catalog_metric', 'metric', 'a:2:{s:19:\"auto_option_sorting\";N;s:19:\"reference_data_name\";N;}', '2020-03-06 10:41:04', '2020-03-06 10:41:04', '');
 SQL;
         /** @var Connection $connection */
         $connection = $this->get('database_connection');
