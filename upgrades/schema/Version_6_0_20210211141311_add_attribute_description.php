59e402b793e7b5da8b36d7d7f7b6dccb8e598c1e
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2021-02-15 14:49:52 +0100

    CPM-143: add default to the description column

--- upgrades/schema/Version_6_0_20210211141311_add_attribute_description.php
+++ upgrades/schema/Version_6_0_20210211141311_add_attribute_description.php
@@ -13,7 +13,6 @@ final class Version_6_0_20210211141311_add_attribute_description extends Abstrac
     public function up(Schema $schema) : void
     {
         $this->addSql("ALTER TABLE pim_catalog_attribute ADD description VARCHAR(500) NOT NULL DEFAULT '';");
-        $this->addSql("ALTER TABLE pim_catalog_attribute ALTER COLUMN description DROP DEFAULT;");
     }
 
     public function down(Schema $schema) : void
